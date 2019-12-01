ea1271260ad1215cbcb74bcc805c2df37a735ea7
Marina Ferreira <marina.ferreira.dev@gmail.com>
2019-12-01 14:00:06 -0300

    Updates

---
+++ precache-manifest.422ce84e1ed4a6f1b2d6a6038bf336b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b438c02a122250ef2bd36bfd377ac2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2e94130cd574ef411598",
+    "url": "/monsters-rolodex/static/css/main.3c2e9e46.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "2e94130cd574ef411598",
+    "url": "/monsters-rolodex/static/js/main.afeda551.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
